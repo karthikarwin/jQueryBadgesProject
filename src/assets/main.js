eaa733fa8542b64996e58ee5a92ca26a0c8b1abd
Karthik Arwin <karthik.rosario@gmail.com>
2017-08-19 16:14:19 +0530

    Done
    I haven't completed any course to show. So I did for Badges!

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/karthikarwin.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+	  console.log(response.badges);
+	  addCourses(response.badges);
+    }
+  });
+  
+  function addCourses(courses) {
+	  var $badges = $("#badges");
+	  
+	  courses.forEach(function(course) {
+		 var $course = $("<div />", {
+			'class' : 'course'
+		 }).appendTo(badges);
+		 
+		 $("<h3 />", {
+			text : course.name 
+		 }).appendTo($course);
+		 
+		 $("<img />", {
+			 src: course.badge
+		 }).appendTo($course);
+		 
+		 $("<a />", {
+			 href : course.course_url,
+			 'class' : "btn btn-primary",
+			 target : "_blank",
+			 text : "View Course"
+		 }).appendTo($course);
+	  });
+  }
 
 });
